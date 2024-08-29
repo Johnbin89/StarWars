3daaa4695504aba04873d9cffecf6db1b0c0e9c9
Johnbin89 <46113530+Johnbin89@users.noreply.github.com>
2024-08-29 18:53:47 +0300

    add ModelAdmin for StarWarsCharacter

--- people/admin.py
+++ people/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from people.models import StarWarsCharacter
 # Register your models here.
-admin.site.register(StarWarsCharacter)
+
+class StarWarsCharacterAdmin(admin.ModelAdmin):
+    list_display = ("name", "height", "mass", "birth_year", "home_world_name")
+    search_fields = ['name__icontains', 'home_world_name__icontains']
+
+admin.site.register(StarWarsCharacter, StarWarsCharacterAdmin)
