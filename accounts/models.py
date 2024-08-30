72bddc5ca83a96736a9cf4197f67ec8bba1a2e4d
Johnbin89 <46113530+Johnbin89@users.noreply.github.com>
2024-08-30 14:57:23 +0300

    remove null from favorites M2M/0 on mass

--- accounts/models.py
+++ accounts/models.py
@@ -6,4 +6,4 @@ from people.models import StarWarsCharacter
 
 #https://docs.djangoproject.com/en/5.0/topics/auth/customizing/#using-a-custom-user-model-when-starting-a-project
 class User(AbstractUser):
-    favorites = models.ManyToManyField(StarWarsCharacter, related_name='favorites', default=None, null=True)
+    favorites = models.ManyToManyField(StarWarsCharacter, related_name='favorites', default=None)
--- people/management/commands/load_people.py
+++ people/management/commands/load_people.py
@@ -9,14 +9,14 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         try:
             for i in range(1, 10):
-                result = httpx.get(f'https://swapi.dev/api/people/?page={i}')
+                result = httpx.get(f'https://swapi.dev/api/people/?page={i}', timeout=None)
                 json_response = result.json()
                 people_list = json_response['results']
                 for character in people_list:
                     home_world_name = httpx.get(character['homeworld']).json()['name']
                     birth_year = decimal.Decimal(character['birth_year'][:-3]) if character['birth_year'] != 'unknown' else None
                     height = int(character['height']) if character['height'] != 'unknown' else None
-                    mass = decimal.Decimal(character['mass'].replace(',' ,'')) if character['mass'] != 'unknown' else None
+                    mass = decimal.Decimal(character['mass'].replace(',' ,'')) if character['mass'] != 'unknown' else decimal.Decimal('0')
                     print(f'{character['name']}, {birth_year} ,{home_world_name}, height: {height}, mass = {mass}')
                     starwars_model = StarWarsCharacter(name=character['name'],
                                                        height=height,
