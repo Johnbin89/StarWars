fb3dfb8dcb437279c2a6b2626a21bf6e0f11a93e
Johnbin89 <46113530+Johnbin89@users.noreply.github.com>
2024-08-31 14:36:15 +0300

    whitenoise for admin in prod

--- starwarspeople/settings.py
+++ starwarspeople/settings.py
@@ -68,6 +68,7 @@ if not DEBUG:
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
